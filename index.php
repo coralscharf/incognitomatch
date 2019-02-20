3004951b8216faa26d340cabc014f25fac40d74c
avivf <avivf@campus.technion.ac.il>
2019-02-20 13:39:17 +0200

    add angularjs and bootsrap

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en" ng-app="template" >
+<title>Aviv Test</title>
+<head>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <script src="js/angular.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+    <script src="js/project.js" ></script>
+
+
+    <meta charset="utf-8">
+
+</head>
+<body  ng-controller='ng-cases' ng-init="">
+    
+	<?php
+
+		include "html/home.html";
+
+	?>
+</body>
+</html>
---
+++ js/project.js
@@ -0,0 +1,68 @@
+let app = angular.module('template', []);
+
+app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+		// We can write our own fileUpload service to reuse it in the controller
+		app.service('fileUpload', ['$http', function ($http) {
+			this.uploadFileToUrl = function(file, uploadUrl, name){
+				var fd = new FormData();
+				fd.append('file', file);
+				fd.append('name', name);
+				$http.post(uploadUrl, fd, {
+					transformRequest: angular.identity,
+					headers: {'Content-Type': undefined,'Process-Data': false}
+				}).then(function(data){
+                    let res=data.data;
+                    if (res==="0")
+                    {
+                        console.log("good");
+                        document.getElementById("couldnt_add_new_file").style.display="none";
+                        document.getElementById("added_file_successfully").style.display="block";
+                    }
+                    else {
+                        console.log("not good");
+                        document.getElementById("couldnt_add_new_file").style.display="block";
+                        document.getElementById("added_file_successfully").style.display="none";
+                    }
+                    console.log("res",res );
+
+
+
+					//$scope.uploadFile(data,name);
+
+
+				})
+
+			}
+		}]);
+
+
+//		 myApp.controller('myCtrl', ['$scope', 'fileUpload', function($scope, fileUpload){
+
+app.controller('ng-cases', function ($scope, $http,$compile, $interval, fileUpload, $window, $element, $timeout) {
+
+	$scope.init_case = function () {
+		//$("#nav").show();
+        $scope.hidePages();
+        $("#home").show();
+
+    }; //the function
+
+
+});	 //app.controller
+
+
