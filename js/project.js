6f146b327ee8e24824a9ae195e506a7bda3730b5
avivf <avivf@campus.technion.ac.il>
2019-04-22 13:04:21 +0300

    test get exp

--- js/project.js
+++ js/project.js
@@ -110,6 +110,29 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
             document.getElementById("A_col_instance").innerText=schema[0]['instance'];
         });
 
+        $http({
+            method: 'POST',
+            url: 'php/get_exp_info.php',
+            data: $.param({
+                exp_id: 1,
+                term_a_or_b: 'sch_id_2'
+            }),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(function (data) {
+            console.log((data.data));
+            console.log((data.data)[0]);
+
+            let schema2=data.data;
+            //let index = Math.floor((Math.random() * schema.length) + 1);
+            //console.log(schema[index]);
+            document.getElementById("B_col_name").innerText=schema2[0]['col_name'];
+            document.getElementById("B_col_type").innerText=schema2[0]['col_type'];
+            document.getElementById("B_col_instance").innerText=schema2[0]['instance'];
+        });
+
+
 
     };
 
