a89fd1a83a1caf5a246a81f1c4a4c5700c5cd71c
avivf <avivf@campus.technion.ac.il>
2019-05-04 17:13:52 +0300

    test mouse coordination

--- js/project.js
+++ js/project.js
@@ -252,7 +252,7 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
     };
 
     $scope.captureCoordinate = function($event){
-        $timeout(function() {
+        setInterval(function() {
             console.log("x:" + $event['pageX']+ " y:" + $event['pageY']);
             },3000);
     };
