021f3c85c29d8e3748042ed0ecdf3a79396c8cd4
avivf <avivf@campus.technion.ac.il>
2019-05-10 16:18:42 +0300

    new user form

--- js/project.js
+++ js/project.js
@@ -112,8 +112,8 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
                 u_age: document.getElementById("new_user_age").value,
                 u_occ: document.getElementById("new_user_occ").value,
                 u_edu: document.getElementById("new_user_edu").value,
-                u_woman: $("#new_user_woman").checked,
-                u_man: document.getElementById("new_user_man").value,
+                u_woman: document.getElementById("new_user_gender").value,
+                u_man: document.getElementById("new_user_man").selected,
             }),
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
