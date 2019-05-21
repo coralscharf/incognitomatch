ef19dee07224d8ebf8e406e1090f2ba0d5b402cc
avivf <avivf@campus.technion.ac.il>
2019-05-21 10:17:19 +0300

    new exp form

--- php/new_exp.php
+++ php/new_exp.php
@@ -12,6 +12,47 @@ $connectionInfo = array("UID" => "avivf@avivtest", "pwd" => "1qaZ2wsX!", "Databa
 $serverName = "tcp:avivtest.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
+
+if ($show_instance === false)
+{
+    $show_instance=0;
+}
+else
+{
+    $show_instance=1;
+}
+if ($show_type === false)
+{
+    $show_type=0;
+}
+else
+{
+    $show_type=1;
+}
+if ($show_hierarchy === false)
+{
+    $show_hierarchy=0;
+}
+else
+{
+    $show_hierarchy=1;
+}
+if ($show_feedback === false)
+{
+    $show_feedback=0;
+}
+else
+{
+    $show_feedback=1;
+}
+if ($show_control === false)
+{
+    $show_control=0;
+}
+else
+{
+    $show_control=1;
+}
 $sql="insert into experiments(name, schema_name, num_pairs, disp_instance, disp_type, disp_h, disp_feedback, disp_control) 
 values('$exp_name','$exp_sch_name',$exp_num_pairs,$show_instance,$show_type,$show_hierarchy,$show_feedback,$show_control)";
 
