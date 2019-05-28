bd3419c8b3569d40a53817d3db6a22b319f78c55
avivf <avivf@campus.technion.ac.il>
2019-05-28 11:38:35 +0300

    select pair by order

--- php/get_exp_info.php
+++ php/get_exp_info.php
@@ -11,6 +11,9 @@ $exp_id=stripcslashes($_POST['exp_id']);
 $term_a_or_b=stripcslashes($_POST['term_a_or_b']);
 $index_from_a = stripcslashes($_POST['index_from_a']);
 $order = stripcslashes($_POST['order']);
+
+
+
 $connectionInfo = array("UID" => "avivf@avivtest", "pwd" => "1qaZ2wsX!", "Database" => "avivtest", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:avivtest.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
@@ -24,7 +27,8 @@ else
 {
     $sql="select * from exp_pairs where exp_id=$exp_id and [order] = $order";
 }
-
+echo $sql;
+die();
 $getResults= sqlsrv_query($conn, $sql);
 if ($getResults == FALSE)
     return (sqlsrv_errors());
