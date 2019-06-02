8ddf9d8f8d5dbe96afa27209390eed724de23034
avivf <avivf@campus.technion.ac.il>
2019-06-02 19:10:58 +0300

    select pair by order

--- php/get_exp_info.php
+++ php/get_exp_info.php
@@ -25,7 +25,7 @@ if ($term_a_or_b == 'sch_id_2')
 }
 else
 {
-    if (strpos($excludes, 'and') !== false)
+    if (strpos($excludes, 'and') == false)
     {
         $sql="select * from exp_pairs where exp_id=$exp_id and [order] = $order";
     }
