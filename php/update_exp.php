7ba2f40a7f15cbc349174d3a8a7034916609c622
avivf <avivf@campus.technion.ac.il>
2019-06-08 17:08:44 +0300

    update exp modal

--- php/update_exp.php
+++ php/update_exp.php
@@ -21,8 +21,8 @@ for( $i=0; $i<sizeof($exp_id);$i++)
     disp_feedback = ".$exp_id[$i]['disp_feedback'].",
     disp_control = ".$exp_id[$i]['disp_control'].",
     is_active = ".$exp_id[$i]['is_active']."
-    where id=$exp_id[$i]['id']";
-    echo $sql;
+    where id=".$exp_id[$i]['id'];
+    
     $getResults= sqlsrv_query($conn, $sql);
     if ($getResults == FALSE)
     {
