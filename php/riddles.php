8bc3fc21102b6421531f38f23e94a3741e23b8ab
avivf <avivf@campus.technion.ac.il>
2019-08-21 19:03:56 +0300

    for roee - choose 5 random riddles to show

--- php/riddles.php
+++ php/riddles.php
@@ -18,6 +18,35 @@ $serverName = "tcp:avivtest.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 
+if($riddle_1 === null)
+{
+    $riddle_1=-1;
+}
+if($riddle_2 === null)
+{
+    $riddle_2=-1;
+}
+if($riddle_3 === null)
+{
+    $riddle_3=-1;
+}
+if($riddle_4 === null)
+{
+    $riddle_4=-1;
+}
+if($riddle_5=== null)
+{
+    $riddle_5=-1;
+}
+if($riddle_6=== null)
+{
+    $riddle_6=-1;
+}
+if($riddle_7 === null)
+{
+    $riddle_7=-1;
+}
+
 
 $sql="insert into riddles(user_id, riddle_1, riddle_2, riddle_3, riddle_4, riddle_5, riddle_6, riddle_7, time) 
 values($user_id,$riddle_1,$riddle_2,$riddle_3,$riddle_4,$riddle_5,$riddle_6,$riddle_7,getdate())";
