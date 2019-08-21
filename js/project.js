bb361d787ba2024ecf289c4e6786b98a0f804d28
avivf <avivf@campus.technion.ac.il>
2019-08-21 19:31:27 +0300

    for roee - choose 5 random riddles to show

--- js/project.js
+++ js/project.js
@@ -132,7 +132,15 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
 
     $scope.show_riddle = function () {
         //this function show the riddles div after the user read the instruction.
+        $("#riddle_1").show();
+        $("#riddle_2").show();
+        $("#riddle_3").show();
+        $("#riddle_4").show();
+        $("#riddle_5").show();
+        $("#riddle_6").show();
+        $("#riddle_7").show();
         $("#riddle").show();
+
         let hide1=Math.floor((Math.random() * 7) + 1);
         let choose = false;
         let hide2;
