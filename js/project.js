2aa8a8cf34c86a3d1ba600c070ae91412606f329
coral <coralscharf@gmail.com>
2019-11-04 11:49:12 +0200

    Add correct answers bar to statistics

--- js/project.js
+++ js/project.js
@@ -1191,7 +1191,10 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
                     data: {
                         labels: xLabels,
                         datasets: [{
+                            label: '# of Correct Answers',
                             data: yData,
+                            backgroundColor: colors,
+                            borderWidth: 1
                         }]
                     }
                 });
@@ -1203,50 +1206,6 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
             }
         });
 
-        /*const ctx = document.getElementById("correctAnswersBar").getContext("2d");
-
-        if ($scope.correctAnswersBar){
-            $scope.correctAnswersBar.destroy();
-        }
-
-        $scope.correctAnswersBar = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-
-        document.getElementById("correctAnswersBar").innerHTML = $scope.correctAnswersBar;*/
     };
 
 
