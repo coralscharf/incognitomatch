6c3723210eb4f1bb1b28aa59c99da94fa846824f
coral <coralscharf@gmail.com>
2019-12-04 19:54:35 +0200

    add background image to heat map

--- index.php
+++ index.php
@@ -40,9 +40,9 @@
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/heatmap.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/stock/highstock.js"></script>
+    <!--<script src="https://code.highcharts.com/stock/highstock.js"></script>
     <script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
-    <script type="text/javascript" src="https://www.highcharts.com/samples/data/usdeur.js"></script>
+    <script type="text/javascript" src="https://www.highcharts.com/samples/data/usdeur.js"></script>-->
 
 </head>
 <body  ng-controller='avivTest' ng-init="init_avivTest()">
