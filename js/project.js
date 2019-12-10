c2c63a3f112789330744f17e42b173aaf0d9900b
coral <coralscharf@gmail.com>
2019-12-10 15:21:12 +0200

    add sql for getting mouse data

--- js/project.js
+++ js/project.js
@@ -1306,14 +1306,21 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
                 const all_clicks_list = all_clicks_for_q.split(';');
                 // console.log(all_clicks_list);
                 for (let i_click in all_clicks_list){
-                    let click = JSON.parse((all_clicks_list[i_click].replace('(','['))
-                                            .replace(')',']'));
-                    if( (click[1],click[2]) in $scope.allClicks ){
-                        $scope.allClicks[(click[1],click[2])] += 1;
+                    try {
+                        let click = JSON.parse((all_clicks_list[i_click].replace('(','['))
+                            .replace(')',']'));
+                        if( (click[1],click[2]) in $scope.allClicks ){
+                            $scope.allClicks[(click[1],click[2])] += 1;
+                        }
+                        else{
+                            $scope.allClicks[(click[1],click[2])] = 1;
+                        }
                     }
-                    else{
-                        $scope.allClicks[(click[1],click[2])] = 1;
+                    catch (e) {
+                        console.log("ERROR");
+                        console.log(all_clicks_list[i_click]);
                     }
+
 
                 }
             }
