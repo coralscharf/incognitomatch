adf8736ae37bbffc04ba96a5db68c72d3cc9be5e
coral <coralscharf@gmail.com>
2019-12-14 17:16:25 +0200

    compute heat map matrix

--- js/project.js
+++ js/project.js
@@ -118,7 +118,6 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
 
         $scope.confidenceLineGraph = "";
         $scope.timeBarGraph = "";
-        $scope.get_mouse_click_data();
         $scope.create_heat_map();
     };
 
@@ -1408,81 +1407,84 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
             yLabels.push(y);
         }
 
-        Highcharts.chart('heatMap_container', {
+        $scope.get_mouse_click_data(function() {
+            Highcharts.chart('heatMap_container', {
 
-            chart: {
-                type: 'heatmap',
-                marginTop: 40,
-                marginBottom: 80,
-                plotBorderWidth: 1,
-                plotBackgroundImage: 'https://www.highcharts.com/samples/graphics/skies.jpg'
-            },
+                chart: {
+                    type: 'heatmap',
+                    marginTop: 40,
+                    marginBottom: 80,
+                    plotBorderWidth: 1,
+                    plotBackgroundImage: 'https://www.highcharts.com/samples/graphics/skies.jpg'
+                },
 
 
-            title: {
-                text: 'Sales per employee per weekday'
-            },
+                title: {
+                    text: 'Sales per employee per weekday'
+                },
 
-            xAxis: {
-                categories: xLabels
-                //categories: ['Alexander', 'Marie', 'Maximilian', 'Sophia', 'Lukas', 'Maria', 'Leon', 'Anna', 'Tim', 'Laura']
-            },
+                xAxis: {
+                    categories: xLabels
+                    //categories: ['Alexander', 'Marie', 'Maximilian', 'Sophia', 'Lukas', 'Maria', 'Leon', 'Anna', 'Tim', 'Laura']
+                },
 
-            yAxis: {
-                //categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-                categories: yLabels,
-                title: true,
-                reversed: false
-            },
+                yAxis: {
+                    //categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                    categories: yLabels,
+                    title: true,
+                    reversed: false
+                },
 
-            colorAxis: {
-                min: 0,
-                minColor: '#ffc8bf',
-                maxColor: '#ff1800'
-            },
+                colorAxis: {
+                    min: 0,
+                    minColor: '#ffc8bf',
+                    maxColor: '#ff1800'
+                },
 
-            legend: {
-                align: 'right',
-                layout: 'vertical',
-                margin: 0,
-                verticalAlign: 'top',
-                y: 25,
-                symbolHeight: 280
-            },
+                legend: {
+                    align: 'right',
+                    layout: 'vertical',
+                    margin: 0,
+                    verticalAlign: 'top',
+                    y: 25,
+                    symbolHeight: 280
+                },
 
-            series: [{
-                name: 'Sales per employee',
-                borderWidth: 0,
-                opacity: 0.5,
-                data: $scope.arrDataForHeatMap
-                // data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91]],
-                //dataLabels: {
-                  //  enabled: false,
+                series: [{
+                    name: 'Sales per employee',
+                    borderWidth: 0,
+                    opacity: 0.5,
+                    data: $scope.arrDataForHeatMap
+                    // data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91]],
+                    //dataLabels: {
+                    //  enabled: false,
                     //color: '#000000'
-                //}
-            }],
+                    //}
+                }],
 
-            responsive: {
-                rules: [{
-                    condition: {
-                        maxWidth: 500
-                    },
-                    chartOptions: {
-                        yAxis: {
-                            labels: {
-                                formatter: function () {
-                                    return this.value.charAt(0);
+                responsive: {
+                    rules: [{
+                        condition: {
+                            maxWidth: 500
+                        },
+                        chartOptions: {
+                            yAxis: {
+                                labels: {
+                                    formatter: function () {
+                                        return this.value.charAt(0);
+                                    }
                                 }
                             }
                         }
-                    }
-                }]
-            }
+                    }]
+                }
 
+            });
         });
+
     };
 
-    $scope.get_mouse_click_data = function(){
+    $scope.get_mouse_click_data = function(callback){
         $scope.allClicks = {};
         $http({
             method: 'POST',
@@ -1546,6 +1548,7 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
                 }
                 i += 1;
             }
+            callback();
 
         });
     };
