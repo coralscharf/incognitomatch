9c379fff5bd2ddc7428ff0543f1fb78dfab2b9bf
coral <coralscharf@gmail.com>
2019-12-15 10:21:44 +0200

    update css of heat map

--- js/project.js
+++ js/project.js
@@ -1465,7 +1465,7 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
                     }
                 },
 
-                series: [{
+                series: {
                     name: 'Mouse Location',
                     borderWidth: 0,
                     opacity: 0.5,
@@ -1480,7 +1480,7 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
                     //  enabled: false,
                     //color: '#000000'
                     //}
-                }],
+                },
 
                 responsive: {
                     rules: [{
