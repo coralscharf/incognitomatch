1f74fc93292adb04e06bbfa61b8d21cb4f5edae0
coral <coralscharf@gmail.com>
2020-01-14 16:37:08 +0200

    add text and size to slices

--- js/project.js
+++ js/project.js
@@ -2226,7 +2226,7 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
 
                 let dataVal = [];
                 for (let item in data.data){
-                    const algSim = (data.data)[item]['algSim'];
+                    const algSim = (data.data)[item]['algSim'] * 100;
                     dataVal.push(algSim);
                 }
 
@@ -2254,7 +2254,7 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
                         datasets: [{
                             data: dataVal,
                             backgroundColor: ["#0A00D9", "#FF5C00", "#1BAD00"],
-                            radius: [10,10,20]
+                            radius: [10,10,20],
                         }],
 
                         labels: [
@@ -2272,8 +2272,41 @@ app.controller('avivTest', function ($scope, $http,$compile, $interval, fileUplo
                             display: true,
                             text: 'Time Range as function of number of Questions',
                             fontSize: 18
+                        },
+                        events: false,
+                        animation: {
+                            duration: 500,
+                            easing: "easeOutQuart",
+                            onComplete: function () {
+                                var ctx = this.chart.ctx;
+                                ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
+                                ctx.textAlign = 'center';
+                                ctx.textBaseline = 'bottom';
+
+                                this.data.datasets.forEach(function (dataset) {
+
+                                    for (var i = 0; i < dataset.data.length; i++) {
+                                        var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
+                                            total = dataset._meta[Object.keys(dataset._meta)[0]].total,
+                                            mid_radius = model.innerRadius + (model.outerRadius - model.innerRadius)/2,
+                                            start_angle = model.startAngle,
+                                            end_angle = model.endAngle,
+                                            mid_angle = start_angle + (end_angle - start_angle)/2;
+
+                                        var x = mid_radius * Math.cos(mid_angle);
+                                        var y = mid_radius * Math.sin(mid_angle);
+
+                                        ctx.fillStyle = '#fff';
+                                        if (i == 3){ // Darker text color for lighter background
+                                            ctx.fillStyle = '#444';
+                                        }
+                                        ctx.fillText(dataset.data[i] + "% Similarity", model.x + x, model.y + y);
+                                    }
+                                });
+                            }
                         }
                     }
+
                 });
 
                 document.getElementById("closestMatch").innerHTML = $scope.simAlgInMatch;
